4095ea01b5a8ac27f86af239ec2cafbba676b832
ian waals <161583538+IanWaals@users.noreply.github.com>
2024-05-23 10:29:37 +0200

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,28 @@
+<?php
+
+include ("inc/functionsphp.php");
+
+HtmlHead("Homepage");
+
+?>
+<main>
+    <div class="ButtonLinks">
+        <section class="buttonLink">
+            <img class="minesImage" src="images/Mines.png" alt="image of the casino game mines" width="700">
+            <a href="s"><div class="playNowButton">Play now!</div></a>
+        </section>
+        <img src="images/ads/horizontalAd.png" alt="an ad for coolblue" width="700">
+        <section class="buttonLink">
+            <img class="minesImage" src="images/sportsBet.webp" alt="image of the casino game mines" width="700">
+            <a href=""><div class="playNowButton">Play now!</div></a>
+        </section>
+    </div>
+    <aside class="asideAd">
+        <img class="asideAd" src="images/ads/asideAd.png" alt="an ad for pet food"><br><br><br><br>
+        <img class="asideAd" src="images/ads/asideAd3.png" alt="an ad for pet cars">
+    </aside>
+</main>
+<br>
+
+<?php
+HtmlFoot();
